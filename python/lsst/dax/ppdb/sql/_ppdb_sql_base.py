8649e8ab42290d6cd91daa717175d64af0d2417d
Jeremy McCormick <jeremym@slac.stanford.edu>
2025-12-11 14:18:25 -0800

    Turn on ID generation when loading Felis schema

--- python/lsst/dax/ppdb/sql/_ppdb_sql_base.py
+++ python/lsst/dax/ppdb/sql/_ppdb_sql_base.py
@@ -375,7 +375,10 @@ class PpdbSqlBase:
             table for table in schema_dict.get("tables", []) if table["name"] in filtered_table_names
         ]
 
-        dm_schema: FelisSchema = felis.datamodel.Schema.model_validate(schema_dict)
+        dm_schema: FelisSchema = felis.datamodel.Schema.model_validate(
+            schema_dict,
+            context={"id_generation": True},
+        )
         schema = schema_model.Schema.from_felis(dm_schema)
 
         # Replace schema name with a configured one, just in case it may be
