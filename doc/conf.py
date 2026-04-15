800b6ad5acefcd2521da20c1d9bae9fa4fb68aff
Jeremy McCormick <jeremym@slac.stanford.edu>
2026-04-15 16:49:35 -0500

    Update site configuration

--- doc/conf.py
+++ doc/conf.py
@@ -6,3 +6,8 @@ https://developer.lsst.io/stack/building-single-package-docs.html
 """
 
 from documenteer.conf.guide import *
+
+autodoc_pydantic_model_show_config_summary = False
+autodoc_pydantic_settings_show_config_summary = False
+autodoc_pydantic_settings_show_json = False
+autodoc_pydantic_model_show_json = False
